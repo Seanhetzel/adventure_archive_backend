d7e18da0425dc538a116901ed7713990e7b2ae0a
Sean Hetzel <seanhetzel1@gmail.com>
2019-08-20 15:22:16 -0700

    added index to controller and serializer

--- app/controllers/api/v1/sites_controller.rb
+++ app/controllers/api/v1/sites_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::SitesController < ApplicationController
+    # def index
+    #     endpoint = "https://en.wikipedia.org/w/api.php?"
+    #     site = "el Mirador"
+    #     params = "action=parse&page=#{site}&format=json"
+    #     url = endpoint + params + "&origin=*"
+    #     site_html = Nokogiri::HTML(open(url))
+    #     puts site_html.content
+    # end
+    def index 
+        sites = Site.all 
+        render json: SiteSerializer.new(sites).to_serialized_json
+    end
 end
