b78bc932189305579fcd3fa88cbc53d82e8f236a
Sean Hetzel <seanhetzel1@gmail.com>
2019-08-21 16:57:54 -0700

    got index, create, destroy working with site

--- app/controllers/api/v1/sites_controller.rb
+++ app/controllers/api/v1/sites_controller.rb
@@ -5,4 +5,29 @@ class Api::V1::SitesController < ApplicationController
         render json: SiteSerializer.new(sites).to_serialized_json
     end
 
+    def create
+        site = Site.new(site_params)
+        if site.save!
+            render json: site_params
+        else
+            render :new 
+        end
+    end
+
+    def update
+        @site = Site.find(params[:id])
+        @site.update(site_params)
+        render json: site_params
+    end
+
+    def destroy
+        site = Site.find(params[:id])
+        site.delete
+    end
+
+    private 
+
+    def site_params
+        params.permit(:id, :name, :description)
+    end
 end
