2294a3b3b120f0b1e859445dc526bd8ee1305d44
Sean Hetzel <seanhetzel1@gmail.com>
2019-08-22 18:04:53 -0700

    added create to user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,14 @@
-class Api::V1::UsersController < ApplicationController
-    def index 
-        users = User.all 
-        render json: UserSerializer.new(users).to_serialized_json
+class Api::V1::UsersController < ApplicationController 
+    def create
+        user = User.new(user_params)
+        if user.save!
+            render json: user_params
+        else
+            render :new 
+        end
+    end
+
+    def user_params
+        params.permit(:id, :name)
     end
 end
