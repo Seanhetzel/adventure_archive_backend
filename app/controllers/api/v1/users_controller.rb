236129cce6fd5d4ccf47f1defddebb8da6a21833
Sean Hetzel <seanhetzel1@gmail.com>
2019-08-25 11:57:07 -0700

    idk

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,25 @@
 class Api::V1::UsersController < ApplicationController 
+
+    def index 
+        user = User.all 
+        render json: UserSerializer.new(user).to_serialized_json
+    end
+
     def create
-        user = User.new(user_params)
-        if user.save!
+        if User.exists?(name: params[:name])
+            user = User.find_by(name: params[:name])
             render json: user_params
         else
-            render :new 
+            user = User.new(user_params)
+            if user.save!
+                render json: user_params
+            else
+                render :new 
+            end
         end
     end
 
     def user_params
-        params.permit(:id, :name)
+        params.require(:user).permit(:id, :name)
     end
 end
